11a8ad4ead7f97db70faad4f6b11d5cfa8d21c72
Gian Alingog <gianqalingog@gmail.com>
2026-08-28 21:08:28 +0800

    update template

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -1,14 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define int long long
 #define rep(i, a, b) for(int i = a; i < (b); ++i)
 #define all(x) begin(x), end(x)
 #define sz(x) (int)(x).size()
 typedef long long ll;
-typedef pair<int, int> pii;
 typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> pii;
+typedef vector<pii> vpii;
+
+void solve() {
+	
+}
 
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
 	cin.exceptions(cin.failbit);
+
+	int t = 1;
+	// cin >> t;
+	while (t--) solve();
 }
